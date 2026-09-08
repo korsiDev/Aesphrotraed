838212657f50cc3212e777e6322ed7efd3f53d69
korsiDev <s.enduregames@gmail.com>
2026-09-08 21:39:30 +0200

    Fixed the PlayerDataManager and some data issues. Also removed the reloadPluginConfig, since it isn't needed yet.

--- src/main/java/me/korsidev/aesphrotraed/Aesphrotraed.java
+++ src/main/java/me/korsidev/aesphrotraed/Aesphrotraed.java
@@ -4,30 +4,22 @@ import me.korsidev.aesphrotraed.command.FriendsCommand;
 import me.korsidev.aesphrotraed.command.ServerTitlesCommand;
 import me.korsidev.aesphrotraed.command.SetTitleCommand;
 import me.korsidev.aesphrotraed.data.PlayerDataManager;
-import me.korsidev.aesphrotraed.data.PlayerMemory;
 import me.korsidev.aesphrotraed.events.ChatEvent;
 import me.korsidev.aesphrotraed.events.ChunkCleanupEvent;
 import me.korsidev.aesphrotraed.events.GeneralEvents;
 import me.korsidev.aesphrotraed.util.FriendManager;
 import me.korsidev.aesphrotraed.util.NametagUtility;
-import me.korsidev.aesphrotraed.util.PlayerUtility;
 import me.korsidev.aesphrotraed.util.TitleRegistry;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 public final class Aesphrotraed extends JavaPlugin {
     private NametagUtility nametagUtility;
     private TitleRegistry titleRegistry;
     private GeneralEvents generalEvents;
-    private PlayerDataManager pdManager;
+    private PlayerDataManager playerDataManager;
     private FriendManager friendManager;
     private LuckPerms luckPerms;
 
@@ -40,14 +32,14 @@ public final class Aesphrotraed extends JavaPlugin {
 
         this.titleRegistry = new TitleRegistry(this);
         this.nametagUtility = new NametagUtility(this, titleRegistry);
-        this.pdManager = new PlayerDataManager(this);
+        this.playerDataManager = new PlayerDataManager(this);
         this.friendManager = new FriendManager();
         this.luckPerms = LuckPermsProvider.get();
 
         this.generalEvents = new GeneralEvents(
                 this,
                 nametagUtility,
-                pdManager
+                playerDataManager
         );
 
         Bukkit.getPluginManager().registerEvents(
@@ -55,7 +47,6 @@ public final class Aesphrotraed extends JavaPlugin {
                 this
         );
 
-        Bukkit.getPluginManager().registerEvents(new PlayerDataManager(this), this);
         Bukkit.getPluginManager().registerEvents(new ChatEvent(this), this);
         Bukkit.getPluginManager().registerEvents(new ChunkCleanupEvent(), this);
 
@@ -75,21 +66,6 @@ public final class Aesphrotraed extends JavaPlugin {
         // Plugin startup logic
     }
 
-    public void reloadPluginConfig() {
-        // 1. Lädt die config.yml frisch von der Festplatte in den RAM
-        this.reloadConfig();
-
-        this.nametagUtility.removeAllNametags();
-        org.bukkit.Bukkit.getScheduler().runTaskLater(this, () -> {
-            this.getLogger().info("Generating text-display nametags for online players...");
-            for (org.bukkit.entity.Player player : org.bukkit.Bukkit.getOnlinePlayers()) {
-                this.nametagUtility.createNametag(player);
-            }
-        }, 2L);
-
-        this.getLogger().info("Configuration successfully reloaded!");
-    }
-
     public LuckPerms getLuckPerms() {
         return this.luckPerms;
     }
@@ -100,14 +76,15 @@ public final class Aesphrotraed extends JavaPlugin {
 
     @Override
     public void onDisable() {
+
+        if (this.playerDataManager != null) {
+            this.playerDataManager.saveAll();
+        }
+
         if (this.nametagUtility != null) {
-            this.getLogger().info("Removing all active TextDisplay nametags before shutdown...");
             this.nametagUtility.removeAllNametags();
         }
 
-
         getLogger().info("Plugin has been disabled.");
-
-        // Plugin shutdown logic
     }
 }
--- src/main/java/me/korsidev/aesphrotraed/data/PlayerDataManager.java
+++ src/main/java/me/korsidev/aesphrotraed/data/PlayerDataManager.java
@@ -6,14 +6,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 
-public class PlayerDataManager implements Listener {
+public class PlayerDataManager {
     private final Aesphrotraed plugin;
 
     public PlayerDataManager(Aesphrotraed plugin) {
@@ -66,6 +65,54 @@ public class PlayerDataManager implements Listener {
         return memory;
     }
 
+    private void loadData(PlayerMemory memory, FileConfiguration config) {
+        memory.setBalance(config.getLong("stats.balance", 100));
+
+        memory.setCurrentStreak(
+                config.getInt("stats.currentStreak", 0)
+        );
+        memory.setLongestStreak(
+                config.getInt("stats.longestStreak", 0)
+        );
+        memory.setTotalGamesPlayed(
+                config.getInt("stats.totalGamesPlayed", 0)
+        );
+        memory.setTotalLoss(
+                config.getLong("stats.totalLoss", 0)
+        );
+        memory.setTotalWagered(
+                config.getLong("stats.totalWagered", 0)
+        );
+        memory.setTotalWon(
+                config.getLong("stats.totalWon", 0)
+        );
+
+        memory.setFriends(
+                config.getStringList("other.friends")
+        );
+
+        memory.setOwnedTitles(
+                config.getStringList("titles.ownedTitles")
+        );
+
+        if (memory.getOwnedTitles().isEmpty()) {
+            memory.getOwnedTitles().add("newbie");
+        }
+
+        memory.setEquippedTitle(
+                config.getString("titles.equippedTitle", "newbie")
+        );
+    }
+
+    public void savePlayer(Player player) {
+        PlayerMemory memory = PlayerUtility.getPlayerMemory(player);
+
+        if (memory == null) {
+            return;
+        }
+
+        savePlayer(player, memory);
+    }
     public void savePlayer(Player player, PlayerMemory memory) {
         File playerFolder =
                 new File(PlayerUtility.getFolderPath(player));
@@ -111,80 +158,5 @@ public class PlayerDataManager implements Listener {
             savePlayer(player);
         }
     }
-
-    /*public void savePlayer(Player player) {
-        PlayerMemory memory = PlayerUtility.getPlayerMemory(player);
-        File f = new File(PlayerUtility.getFolderPath(player) + "/general.yml");
-        FileConfiguration cfg = YamlConfiguration.loadConfiguration(f);
-
-        cfg.set("stats.coins", memory.getBalance());
-        cfg.set("stats.currentStreak", memory.getCurrentStreak());
-        cfg.set("stats.longestStreak", memory.getLongestStreak());
-        cfg.set("stats.totalGamesPlayed", memory.getTotalGamesPlayed());
-        cfg.set("stats.totalLoss", memory.getTotalLoss());
-        cfg.set("stats.totalWagered", memory.getTotalWagered());
-        cfg.set("stats.totalWon", memory.getTotalWon());
-        cfg.set("other.friends", memory.getFriends());
-        cfg.set("titles.ownedTitles", memory.getOwnedTitles());
-        cfg.set("titles.equippedTitle", memory.getEquippedTitle());
-
-        try {
-            cfg.save(f);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        PlayerUtility.setPlayerMemory(player, null);
-    }
-
-    public void loadPlayer(Player player) {
-        PlayerMemory memory = new PlayerMemory();
-        File f = new File(PlayerUtility.getFolderPath(player) + "/general.yml");
-
-        File playerFolder = new File(PlayerUtility.getFolderPath(player));
-
-        if (!playerFolder.exists() && !playerFolder.mkdirs()) {
-            plugin.getLogger().severe(
-                    "Could not create player folder for " + player.getName()
-            );
-        }
-
-        if(f.exists()){
-            FileConfiguration cfg = YamlConfiguration.loadConfiguration(f);
-            memory.setBalance(cfg.getLong("stats.balance"));
-            memory.setCurrentStreak(cfg.getInt("stats.currentStreak"));
-            memory.setLongestStreak(cfg.getInt("stats.longestStreak"));
-            memory.setTotalGamesPlayed(cfg.getInt("stats.totalGamesPlayed"));
-            memory.setTotalLoss(cfg.getLong("stats.totalLoss"));
-            memory.setTotalWagered(cfg.getLong("stats.totalWagered"));
-            memory.setTotalWon(cfg.getLong("stats.totalWon"));
-            memory.setFriends(cfg.getStringList("other.friends"));
-            memory.setOwnedTitles(cfg.getStringList("titles.ownedTitles"));
-            if (!memory.getOwnedTitles().contains("newbie")) {
-                memory.getOwnedTitles().add("newbie");
-            }
-
-            memory.setEquippedTitle(cfg.getString("titles.equippedTitle"));
-        } else {
-            memory.setBalance(100);
-            memory.setCurrentStreak(0);
-            memory.setLongestStreak(0);
-            memory.setTotalGamesPlayed(0);
-            memory.setTotalLoss(0);
-            memory.setTotalWagered(0);
-            memory.setTotalWon(0);
-            memory.setFriends(new ArrayList<>());
-            memory.setOwnedTitles(new ArrayList<>());
-            memory.getOwnedTitles().add("newbie");
-            memory.setEquippedTitle("§aNewbie");
-        }
-
-        PlayerUtility.setPlayerMemory(player, memory);
-
-        Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            plugin.getNametagUtility().createNametag(player);
-        }, 2L);
-
-    }*/
 
 }
